b8dbd0adeb8f0847fca4117261ddffabf41621f7
mkujun <kujundzic.marko@hotmail.com>
2023-09-25 21:22:33 +0200

    read youtube links from file

--- index.js
+++ index.js
@@ -1,23 +1,52 @@
 const readline = require('readline');
 const ytdl = require('ytdl-core');
 const ffmpeg = require('fluent-ffmpeg');
+const fs = require('fs');
+const filePath = 'input.txt';
 
-let id = 'fJ1gPPDCCDU';
+function openStream(line) {
+  let id = line.substring(32, 43);
 
-let stream = ytdl(id, {
-  quality: 'highestaudio',
+  if (id) {
+    let stream = ytdl(id, {
+      quality: 'highestaudio',
+    });
+
+    let start = Date.now();
+    ffmpeg(stream)
+      .audioBitrate(128)
+      .save(`${__dirname}/${id}.mp3`)
+      .on('progress', p => {
+        readline.cursorTo(process.stdout, 0);
+        process.stdout.write(`${p.targetSize}kb downloaded`);
+      })
+      .on('end', () => {
+        console.log(`\ndone, thanks - ${(Date.now() - start) / 1000}s`);
+      });
+  }
+}
+
+// Create a readable stream to read the file
+const stream = fs.createReadStream(filePath, { encoding: 'utf8' });
+
+// Event handler for when data is read from the file
+stream.on('data', (data) => {
+  const lines = data.split('\n');
+
+  // Process each line
+  lines.forEach((line) => {
+    //console.log(line);
+    openStream(line);
+  });
 });
 
-let start = Date.now();
+// Event handler for when the file has been completely read
+stream.on('end', () => {
+  console.log('File reading finished.');
+});
 
-ffmpeg(stream)
-  .audioBitrate(128)
-  .save(`${__dirname}/${id}.mp3`)
-  .on('progress', p => {
-    readline.cursorTo(process.stdout, 0);
-    process.stdout.write(`${p.targetSize}kb downloaded`);
-  })
-  .on('end', () => {
-    console.log(`\ndone, thanks - ${(Date.now() - start) / 1000}s`);
-  });
+// Event handler for handling errors
+stream.on('error', (error) => {
+  console.error('Error reading the file:', error);
+});
 
